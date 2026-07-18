0804f3217482358d79b7f43bac2bd1944b8f3351
David <Daoyizi323@gmail.com>
2026-07-18 18:32:43 +0800

    Initial Commit

---
+++ MDown View/MDown_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MDown_ViewApp.swift
+//  MDown View
+//
+//  Created by 道一 on 2026/7/18.
+//
+
+import SwiftUI
+
+@main
+struct MDown_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
